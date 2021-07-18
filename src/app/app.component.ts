db1e65b5e1129f08f9744209e445aea8d3fb3625
Sujeet Jaiswara <sujeet.jaiswara@taritas.com>
2021-07-18 14:13:15 +0530

    Completed - sample app design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-pwa-demo';
+
+  images = [
+    { url: "https://picsum.photos/id/10/200/300" },
+    { url: "https://picsum.photos/id/1002/200/300" },
+    { url: "https://picsum.photos/id/1011/200/300" },
+    { url: "https://picsum.photos/id/1015/200/300" },
+    { url: "https://picsum.photos/id/593/200/300" },
+    { url: "https://picsum.photos/id/1039/200/300" },
+    { url: "https://picsum.photos/id/163/200/300" },
+    { url: "https://picsum.photos/id/168/200/300" },
+    { url: "https://picsum.photos/id/360/200/300" },
+    { url: "https://picsum.photos/id/376/200/300" },
+    { url: "https://picsum.photos/id/400/200/300" },
+    { url: "https://picsum.photos/id/401/200/300" },
+    { url: "https://picsum.photos/id/428/200/300" },
+    { url: "https://picsum.photos/id/431/200/300" },
+    { url: "https://picsum.photos/id/525/200/300" },
+    { url: "https://picsum.photos/id/63/200/300" },
+    { url: "https://picsum.photos/id/631/200/300" },
+    { url: "https://picsum.photos/id/696/200/300" },
+    { url: "https://picsum.photos/id/701/200/300" },
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
@@ -23,6 +24,7 @@ import { environment } from '../environments/environment';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    MatGridListModule,
 
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
